18c8b3dfc9899f0d441f95c337d2b57fb813b83c
Joel Curtis <45494575+joelcurtis19@users.noreply.github.com>
2021-11-15 19:38:28 -0500

    Add files via upload

---
+++ Code/1_load_clean.R
@@ -0,0 +1,112 @@
+rm(list=ls())
+
+library(tidyverse)
+library(googledrive)
+library(data.table)
+
+root_path <- "C:/Users/Joel/Documents/Kaggle/March Madness/Men/2021/Stage_2/"
+
+input_path <- paste(root_path, "Input/", sep = "")
+file_names <- list.files(path = input_path, pattern = "*.csv")
+
+output_path <- paste(root_path, "Output/", sep = "")
+
+files <- map(file_names, ~paste(input_path, .x, sep = ""))
+
+
+file_list <- map(files, read_csv)
+
+file_names <- map(file_names, ~gsub(".csv","" , .x))
+
+names(file_list) <- file_names
+
+#cities <- file_list$Cities
+conferences <- file_list$Conferences
+conferenceTourneyGames <- file_list$MConferenceTourneyGames
+#gameCities <- file_list$MGameCities
+#ncaaTourneyCompactResults <- file_list$MNCAATourneyCompactResults
+ncaaTourneyDetailedResults <- file_list$MNCAATourneyDetailedResults
+#ncaaTourneySeedRoundSlots <- file_list$MNCAATourneySeedRoundSlots
+ncaaTourneySeeds <- file_list$MNCAATourneySeeds
+#ncaaTourneySlots <- file_list$MNCAATourneySlots
+#regularSeasonCompactResults <- file_list$MRegularSeasonCompactResults
+regularSeasonDetailedResults <- file_list$MRegularSeasonDetailedResults
+#sampleSubmssionStage2 <- file_list$MSampleSubmissionStage2
+#seasons <- file_list$MSeasons
+#secondaryTourneyCompactResults <- file_list$MSecondaryTourneyCompactResults
+#secondaryTourneyTeams <- file_list$MSecondaryTourneyTeams
+#teamCoaches <- file_list$MTeamCoaches
+teamConferences <- file_list$MTeamConferences
+teams <- file_list$MTeams
+#teamSpellings <- file_list$MTeamSpellings
+
+ncaaTourneySeeds <- ncaaTourneySeeds %>%
+  mutate(Season_TeamID = paste(Season, TeamID, sep = "_"),
+         Seed = parse_number(Seed))%>%
+  select(-Season, -TeamID)
+
+teamConferences <- teamConferences %>%
+  left_join(conferences, by = "ConfAbbrev")%>%
+  mutate(Season_TeamID = paste(Season, TeamID, sep = "_"))%>%
+  select(-Season, -TeamID)
+
+teams <- teams %>%
+  select(TeamID, TeamName)
+
+conferenceTourneyGames <- conferenceTourneyGames %>%
+  mutate(Season_Conf_Day = paste(Season, ConfAbbrev, DayNum, sep = "_"),
+         Competition = "Conf_Tourney")%>%
+  select(Season_Conf_Day, Competition)
+
+#join tourney and regular season results with seeds and teams
+
+
+clean_detailed_results <- function(.data){
+  
+  .data <- .data %>%
+    mutate(WSeason_TeamID = paste(Season, WTeamID, sep = "_"),
+           LSeason_TeamID = paste(Season, LTeamID, sep = "_"))%>%
+    left_join(ncaaTourneySeeds, by = c("WSeason_TeamID" = "Season_TeamID"))%>%
+    rename(WSeed = Seed)%>%
+    left_join(ncaaTourneySeeds, by = c("LSeason_TeamID" = "Season_TeamID"))%>%
+    rename(LSeed = Seed)%>%
+    left_join(teams, by = c("WTeamID" = "TeamID"))%>%
+    rename(WTeamName = TeamName)%>%
+    left_join(teams, by = c("LTeamID" = "TeamID"))%>%
+    rename(LTeamName = TeamName)%>%
+    left_join(teamConferences, by = c("WSeason_TeamID" = "Season_TeamID"))%>%
+    rename(WConfAbbrev = ConfAbbrev, WConference = Description)%>%
+    left_join(teamConferences, by = c("LSeason_TeamID" = "Season_TeamID"))%>%
+    rename(LConfAbbrev = ConfAbbrev, LConference = Description)%>%
+    mutate(Seed_Diff = WSeed-LSeed,
+           Upset = ifelse(Seed_Diff>=3,TRUE,FALSE))
+  
+  .data
+
+}
+
+
+ncaaTourneyDetailedResults <- ncaaTourneyDetailedResults %>%
+  clean_detailed_results()%>%
+  mutate(Competition = "NCAA_Tourney")
+  
+regularSeasonDetailedResults <- regularSeasonDetailedResults%>%
+  clean_detailed_results()%>%
+  mutate(ConfAbbrev = ifelse(WConfAbbrev == LConfAbbrev, WConfAbbrev, NA),
+         Season_Conf_Day = paste(Season, ConfAbbrev, DayNum, sep = "_"))%>%
+  left_join(conferenceTourneyGames, by = "Season_Conf_Day")%>%
+  mutate(Competition = ifelse(is.na(Competition),
+                              "Regular_Season", Competition))%>%
+  select(-c(Season_Conf_Day, ConfAbbrev))
+  
+
+
+detailedResults <- bind_rows(regularSeasonDetailedResults, ncaaTourneyDetailedResults)
+
+detailed_path <- paste(output_path, "detailedResults.csv", sep = "")
+
+write_csv(detailedResults, detailed_path)
+
+#drive_put(detailed_path,
+#          path = "March Madness/Men/2021/Output/detailedResults.csv",
+#          type = "spreadsheet")
---
+++ Code/3_prepare_predict.R
@@ -0,0 +1,82 @@
+rm(list=ls())
+
+library(tidyverse)
+
+root_path <- "C:/Users/Joel/Documents/Kaggle/March Madness/Men/2021/Stage_2/"
+
+
+output_path <- paste(root_path, "Output/", sep = "")
+
+regular_stats_path <- paste(root_path, "Output/regularConfTourneyStats.csv", sep = "")
+samp_sub_path <- paste(root_path, "Input/MSampleSubmissionStage2.csv", sep = "")
+
+regularConfTourneyStats <- read_csv(regular_stats_path)
+samp_sub <- read_csv(samp_sub_path)
+
+
+stats_drop <- c("Year",
+                "TeamID")
+
+teamAStats <- regularConfTourneyStats%>%
+  select(-all_of(stats_drop))
+  
+teamBStats <- regularConfTourneyStats%>%
+  select(-all_of(stats_drop))
+
+names(teamAStats) <- paste("TeamA", names(teamAStats), sep = "_")
+names(teamBStats) <- paste("TeamB", names(teamBStats), sep = "_")
+
+
+predict_df <- samp_sub %>%
+  mutate(Year = str_sub(ID, start = 1L, end = -11L),
+         TeamA_ID = str_sub(ID, start = 6L, end = -6L),
+         TeamB_ID = str_sub(ID, start = 11L, end = -1L),
+         TeamA_Season_TeamID = paste(Year, TeamA_ID, sep = "_"),
+         TeamB_Season_TeamID = paste(Year, TeamB_ID, sep = "_"))%>%
+  left_join(teamAStats, by = c("TeamA_Season_TeamID" = "TeamA_Season_TeamID"))%>%
+  left_join(teamBStats, by = c("TeamB_Season_TeamID" = "TeamB_Season_TeamID"))%>%
+  select(-ends_with("_median"), -ends_with("_sd"))%>%
+  distinct_at("ID", .keep_all = TRUE)
+
+##############################################################
+#### calculate difference between Team A and Team B stats ####
+##############################################################
+
+predict_drop <- c("ID",
+                  "Pred",
+                  "Year",
+                  "TeamA_ID",
+                  "TeamA_Season_TeamID",
+                  "TeamB_ID",
+                  "TeamB_Season_TeamID")
+
+predictA <- predict_df %>%
+  select(starts_with("TeamA"),
+         -all_of(predict_drop),
+         -ends_with("_median"),
+         -ends_with("_sd"))
+
+predictB <- predict_df %>%
+  select(starts_with("TeamB"),
+         -all_of(predict_drop),
+         -ends_with("_median"),
+         -ends_with("_sd"))
+
+predict_diff <- predictA - predictB
+
+names(predict_diff) <- names(predict_diff) %>%
+  gsub("TeamA_*", "",.)%>%
+  gsub("*_mean", "", .)
+
+predict <- predict_df%>%
+  select(ID, Year, TeamA_ID, TeamB_ID, Pred,
+         TeamA_Season_TeamID, TeamB_Season_TeamID)
+
+predict <- predict %>%
+  bind_cols(predict_diff)
+
+#####export predict.csv
+
+predict_path <- paste(output_path, "predict.csv", sep = "")
+
+write_csv(predict, predict_path)
